6335943ff0ac8a511ca7a16a99e2645f0ccec5e5
Cyanineee-24 <scoutchampion123@gmail.com>
2025-11-25 23:30:58 +0800

    Nov_11_25: Temporary Forms Layout pushed (Fields still at default). TODO: change fieldNames, furnish functionality)

---
+++ src/Home/HOME.java
@@ -0,0 +1,86 @@
+package Home;
+
+import javax.swing.*;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class HOME extends JFrame {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JProgressBar progressBar1;
+    private JTextField enterFullNameTextField;
+    private JTextField enterEmailAddressTextField;
+    private JTextField enterAgeTextField;
+    private JComboBox comboBox2;
+    private JButton VVA1Button;
+    private JButton VVA10Button;
+    private JButton VVA2Button;
+    private JButton VVA3Button;
+    private JButton VVA4Button;
+    private JButton VVA5Button;
+    private JButton VVA6Button;
+    private JButton VVA9Button;
+    private JButton VVA8Button;
+    private JButton VVA7Button;
+    private JScrollBar scrollBar1;
+    private JButton vB1Button2;
+    private JButton vB2Button1;
+    private JButton vB3Button1;
+    private JButton vB4Button1;
+    private JButton vB5Button1;
+    private JButton vB6Button1;
+    private JButton vB8Button1;
+    private JButton vB9Button1;
+    private JButton vB10Button;
+    private JButton vB7Button1;
+    private JButton vC1Button;
+    private JButton vC2Button;
+    private JButton vC3Button;
+    private JButton vC4Button;
+    private JButton vC5Button;
+    private JButton vC6Button;
+    private JButton vC7Button;
+    private JButton vC8Button;
+    private JButton vC9Button;
+    private JButton vC10Button;
+    private JButton gD1Button;
+    private JButton gD2Button;
+    private JButton gD3Button;
+    private JButton gD4Button;
+    private JButton gD5Button;
+    private JButton gD6Button;
+    private JButton gD7Button;
+    private JButton gD8Button;
+    private JButton gD9Button;
+    private JButton gD10Button;
+    private JButton gE1Button;
+    private JButton gE2Button;
+    private JButton gE3Button;
+    private JButton gE4Button;
+    private JButton gE5Button;
+    private JButton gE6Button;
+    private JButton gE7Button;
+    private JButton gE8Button;
+    private JButton gE9Button;
+    private JButton g10Button;
+    private JButton gF1Button;
+    private JButton gF2Button;
+    private JButton gF3Button;
+    private JButton gF4Button;
+    private JButton gF5Button;
+    private JButton gF6Button;
+    private JButton gF7Button;
+    private JButton gF8Button;
+    private JButton gF9Button;
+    private JButton gF10Button;
+    private JButton CONFIRMTICKETDETAILSButton;
+
+    public HOME() {
+        scrollBar1.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+
+            }
+        });
+    }
+}
