e2cd0e5b4172fbe2377953c01a865afd92b6fa3c
kworuu <andrecamasura144@gmail.com>
2025-11-29 23:48:50 +0800

    Added getters for private fields in Seat class

--- src/ConcertAdmissionSystem/Seat.java
+++ src/ConcertAdmissionSystem/Seat.java
@@ -1,10 +1,8 @@
 package ConcertAdmissionSystem;
 
-import java.time.LocalDate;
-
 public class Seat {
-    private String seatNumber;
-    private String row;
+    private final String seatNumber;
+    private final String row;
     private boolean isTaken;
     private SeatingTier tier;
 
@@ -12,19 +10,38 @@ public class Seat {
 //        super(price, concert, seating, seat, timeBought, tierName, basePrice, perks, maxCapacity, currentSold);
         this.seatNumber = seatNumber;
         this.row = row;
-        this.isTaken = isTaken;
+        this.isTaken = false;
         this.tier = tier;
     }
 
-    void markAsTaken(){
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public boolean isTaken() {
+        return isTaken;
+    }
+
+    public SeatingTier getTier() {
+        return tier;
+    }
+
+    public void markAsTaken(){
+        if(isTaken) {
+            throw new IllegalStateException("Seat " + seatNumber + " is already taken");
+        }
         isTaken = true;
     }
 
-    boolean isAvailable(){
-        return false;
+    public boolean isAvailable(){
+        return !isTaken;
     }
 
-    String getSeatInfo() {
+    public String getSeatInfo() {
         return "Row " + row + ", Seat " + seatNumber + " [" + tier.getTierInfo() + "] - " + (isTaken ? "Taken" : "Available");
     }
 
